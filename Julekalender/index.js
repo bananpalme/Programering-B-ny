3a960bfb723dc34bbe43ffbc8bb24599b58e826f
bananpalme <77321863+bananpalme@users.noreply.github.com>
2021-12-06 21:51:40 +0100

    Add files via upload

--- Julekalender/index.js
+++ Julekalender/index.js
@@ -1,3 +1,9 @@
+var epico = new Audio('./assets/epic.mp3');
+var goldlego = new Audio('./assets/gold leg.mp3')
+var legendaryo = new Audio('./assets/legendary.mp3')
+var rareo = new Audio('./assets/rare.mp3')
+var chest = new Audio('./assets/chest.mp3')
+
 fetch('./kalender.json')
     .then( res => res.json() )
     .then( json => {
@@ -8,21 +14,70 @@ fetch('./kalender.json')
             div.classList.add('door')
             div.style.width = door.width
             div.style.height = door.height
-            div.innerHTML = door.date
+            
+            let lock = document.createElement('div')
+            lock.classList.add('lock')
+            lock.innerHTML = door.date
+            
+            lock.addEventListener('click', () => {
+                
+                
+                switch(door.rarity){
+                    case 'rare':
+                        rareo.play()
+                        chest.play()
+                    break
+                    case 'epic':
+                        chest.play()
+                        epico.play()
+                    break
+                    case 'legendary':
+                        legendaryo.play()
+                        chest.play()
+                    break
+                    case 'goldlegendary':
+                        goldlego.play()
+                        chest.play()
+                    break
+                }
 
+                switch(door.animation){
+                    case 'slide':
+                        lock.innerHTML = ''
+                        lock.classList.add('open')
+                    break
+                    case 'golden':
+                         lock.innerHTML = ''
+                        lock.classList.add('goldenopen')
+                    break
+                    case 'legendary':
+                        lock.innerHTML = ''
+                        lock.classList.add('cum')
+                    break
+                    case 'sovs':
+                        lock.innerHTML = ''
+                        lock.classList.add('sovsjohn')
+                    default:
+                        lock.innerHTML = ''
+                        lock.classList.add('open')
+                        break
+                    }
+            })
+                        
             switch(door.content){
                 case 'image': 
                     setImage(div, door)
-                    break
+                break
                 case 'youtube': 
                     setYoutube(div, door)
-                    break
+                break
                 default:
-                    break
-                
+                break
+                                
             }
-
+                            
             document.querySelector('main').append(div)
+            div.append(lock)
         })
     })
 
